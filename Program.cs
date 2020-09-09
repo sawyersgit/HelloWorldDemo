188e147fd56368d2e8b29e2b9cec406199b0166d
Sawyer <sawyer9smith@gmail.com>
2020-09-09 11:08:00 -0400

    Finally Hello World!

--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@ namespace HelloWorldDemo
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello VVorld!");
+        static void Main(string[] args) {
+
+           Console.WriteLine("Hello Sawyer!"); 
+           Console.WriteLine("What is your name?");
+           var input = Console.ReadLine();
+           Console.WriteLine($"Hello, {input}!");
+
         }
+        
+         
+
     }
 }
